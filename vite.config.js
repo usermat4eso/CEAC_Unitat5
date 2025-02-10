e628acabbb4e14e0bd31c2cbae66fcb43007b10c
NandoVerdu <fverdu@sanantonio1.org>
2025-02-10 11:15:27 +0100

    last 2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  outDir: 'dist',
   base: (process.env.NODE_ENV === "production" && process.env.VERCEL != "1") ? "/WebQuiz/" : "/",
   plugins: [vue()],
   resolve: {
